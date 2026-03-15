eef157a6879be0352b786b06e15f2747749bf2b3
Chris Jung <ch83238@gmail.com>
2026-03-15 12:39:42 +0100

    Code strukturiert und Menü-Funktion hinzugefügt

--- Main.py.py
+++ Main.py.py
@@ -1,7 +1,182 @@
-Produkte = []
-Mahlzeiten = []
+def produkt_hinzufuegen():
+    name = input("Produkt Name: ")
 
-while True:
+    while True:
+        try:
+            kalorien = float(input("Kalorien auf 100g: ").replace(",", "."))
+            break
+        except ValueError:
+            print("Bitte eine gültige Zahl eingeben.")
+
+    while True:
+        try:
+            eiweiss = float(input("Eiweiß auf 100g: ").replace(",", "."))
+            break
+        except ValueError:
+            print("Bitte eine gültige Zahl eingeben.")
+
+    while True:
+        try:
+            fett = float(input("Fett auf 100g: ").replace(",", "."))
+            break
+        except ValueError:
+            print("Bitte eine gültige Zahl eingeben.")
+
+    while True:
+        try:
+            kohlenhydrate = float(input("Kohlenhydrate auf 100g: ").replace(",", "."))
+            break
+        except ValueError:
+            print("Bitte eine gültige Zahl eingeben.")
+
+    produkt = {
+        "Name": name,
+        "Kalorien": kalorien,
+        "Eiweiß": eiweiss,
+        "Fett": fett,
+        "Kohlenhydrate": kohlenhydrate
+    }
+
+    produkte.append(produkt)
+    print("Produkt gespeichert.")
+
+
+def mahlzeit_hinzufuegen():
+    mahlzeit_typ = ""
+
+    while True:
+        print("\n1) Frühstück")
+        print("2) Mittagessen")
+        print("3) Abendessen")
+        print("4) Zurück")
+
+        auswahl_mahlzeit = input("Bitte wähle eine Option (1-4): ").strip()
+
+        if auswahl_mahlzeit == "1":
+            mahlzeit_typ = "Frühstück"
+            break
+        elif auswahl_mahlzeit == "2":
+            mahlzeit_typ = "Mittagessen"
+            break
+        elif auswahl_mahlzeit == "3":
+            mahlzeit_typ = "Abendessen"
+            break
+        elif auswahl_mahlzeit == "4":
+            break
+        else:
+            print("Bitte eine gültige Zahl eingeben.")
+
+    if mahlzeit_typ != "":
+        produkte_der_mahlzeit = []
+
+        while True:
+            print("\n1) Produkt auswählen")
+            print("2) Zurück")
+
+            auswahl_produkt = input("Bitte wähle eine Option (1-2): ").strip()
+
+            if auswahl_produkt == "1":
+                user_input_name = input("Produkt Name: ").strip()
+
+                produkt_gefunden = False
+                gespeichertes_produkt = None
+
+                for produkt in produkte:
+                    if produkt["Name"] == user_input_name:
+                        produkt_gefunden = True
+                        gespeichertes_produkt = produkt
+                        break
+
+                if produkt_gefunden:
+                    while True:
+                        try:
+                            gramm = float(input("Gramm: ").replace(",", "."))
+                            break
+                        except ValueError:
+                            print("Bitte eine gültige Zahl eingeben.")
+
+                    mahlzeit_produkt = {
+                        "Name": gespeichertes_produkt["Name"],
+                        "Gramm": gramm
+                    }
+
+                    produkte_der_mahlzeit.append(mahlzeit_produkt)
+                    print("Produkt zur Mahlzeit hinzugefügt.")
+
+                else:
+                    print("Produkt existiert nicht. Bitte zuerst anlegen.")
+
+            elif auswahl_produkt == "2":
+                break
+            else:
+                print("Bitte eine gültige Zahl eingeben.")
+
+        if produkte_der_mahlzeit:
+            mahlzeit = {
+                "Typ": mahlzeit_typ,
+                "Produkte": produkte_der_mahlzeit
+            }
+            mahlzeiten.append(mahlzeit)
+            print("Mahlzeit gespeichert.")
+
+
+def produktliste_anzeigen():
+    if not produkte:
+        print("Keine Produkte gespeichert.")
+    else:
+        print("\nProduktliste:")
+        for produkt in produkte:
+            print(
+                "Name:", produkt["Name"],
+                "| Kalorien:", produkt["Kalorien"],
+                "| Eiweiß:", produkt["Eiweiß"],
+                "| Fett:", produkt["Fett"],
+                "| Kohlenhydrate:", produkt["Kohlenhydrate"]
+            )
+
+
+def mahlzeitenliste_anzeigen():
+    if not mahlzeiten:
+        print("Keine Mahlzeiten gespeichert.")
+    else:
+        print("\nMahlzeitenliste:")
+        for mahlzeit in mahlzeiten:
+            print(mahlzeit["Typ"])
+            for produkt in mahlzeit["Produkte"]:
+                print(
+                    "  Produkt:", produkt["Name"],
+                    "| Gramm:", produkt["Gramm"]
+                )
+
+
+def tageswerte_anzeigen():
+    gesamt_kalorien = 0
+    gesamt_eiweiss = 0
+    gesamt_fett = 0
+    gesamt_kohlenhydrate = 0
+
+    for mahlzeit in mahlzeiten:
+        for produkt in mahlzeit["Produkte"]:
+            for gespeichertes_produkt in produkte:
+                if gespeichertes_produkt["Name"] == produkt["Name"]:
+                    faktor = produkt["Gramm"] / 100
+                    gesamt_kalorien += gespeichertes_produkt["Kalorien"] * faktor
+                    gesamt_eiweiss += gespeichertes_produkt["Eiweiß"] * faktor
+                    gesamt_fett += gespeichertes_produkt["Fett"] * faktor
+                    gesamt_kohlenhydrate += gespeichertes_produkt["Kohlenhydrate"] * faktor
+
+    tageswerte = {
+        "Kalorien": gesamt_kalorien,
+        "Eiweiß": gesamt_eiweiss,
+        "Fett": gesamt_fett,
+        "Kohlenhydrate": gesamt_kohlenhydrate
+    }
+
+    print("\nTageswerte:")
+    for name, wert in tageswerte.items():
+        print(f"{name}: {wert:.2f}")
+
+def menue_anzeigen():
     print("\n1) Produkt hinzufügen")
     print("2) Mahlzeit hinzufügen")
     print("3) Produktliste anzeigen")
@@ -9,184 +184,28 @@ while True:
     print("5) Tageswerte anzeigen")
     print("6) Beenden")
 
+produkte = []
+mahlzeiten = []
+
+while True:
+    menue_anzeigen()
+
     auswahl = input("Bitte wähle eine Option (1-6): ").strip()
 
     if auswahl == "1":
-        Name = input("Produkt Name: ")
+        produkt_hinzufuegen()
 
-        while True:
-            try:
-                Kalorien = float(input("Kalorien auf 100g: ").replace(",", "."))
-                break
-            except ValueError:
-                print("Bitte eine gültige Zahl eingeben.")
-
-        while True:
-            try:
-                Eiweiß = float(input("Eiweiß auf 100g: ").replace(",", "."))
-                break
-            except ValueError:
-                print("Bitte eine gültige Zahl eingeben.")
-
-        while True:
-            try:
-                Fett = float(input("Fett auf 100g: ").replace(",", "."))
-                break
-            except ValueError:
-                print("Bitte eine gültige Zahl eingeben.")
-
-        while True:
-            try:
-                Kohlenhydrate = float(input("Kohlenhydrate auf 100g: ").replace(",", "."))
-                break
-            except ValueError:
-                print("Bitte eine gültige Zahl eingeben.")
-
-        Produkt = {
-            "Name": Name,
-            "Kalorien": Kalorien,
-            "Eiweiß": Eiweiß,
-            "Fett": Fett,
-            "Kohlenhydrate": Kohlenhydrate
-        }
-
-        Produkte.append(Produkt)
-        print("Produkt gespeichert.")
-
-    if auswahl == "2":
-
-        Meal_type = ""
-
-        while True:
-            print("\n1) Frühstück")
-            print("2) Mittagessen")
-            print("3) Abendessen")
-            print("4) Zurück")
-
-            Meal = input("Bitte wählen eine Option (1-4): ").strip()
-
-            if Meal == "1":
-                Meal_type = "Frühstück"
-                break
-
-            elif Meal == "2":
-                Meal_type = "Mittagessen"
-                break
-
-            elif Meal == "3":
-                Meal_type = "Abendessen"
-                break
-
-            elif Meal == "4":
-                break
-
-            else:
-                print("Bitte eine gültige Zahl eingeben.")
-
-        if Meal_type != "":
-
-            Food_type = []
-
-            while True:
-                print("\n1) Produkt Auswählen")
-                print("2) Zurück")
-
-                Food = input("Bitte wähle eine Option (1-2): ").strip()
-
-                if Food == "1":
-                    user_input_name = input("Produkt Name: ").strip()
-
-                    Produkt_gefunden = False
-                    gespeichert_Produkt = None
-
-                    for product in Produkte:
-                        if product["Name"] == user_input_name:
-                            Produkt_gefunden = True
-                            gespeichert_Produkt = product
-                            break
-
-                    if Produkt_gefunden:
-                        while True:
-                            try:
-                                Gramm = float(input("Gramm: ").replace(",", "."))
-                                break
-                            except ValueError:
-                                print("Bitte eine gültige Zahl eingeben.")
-
-                        Mahlzeit_produkt = {
-                            "Name": gespeichert_Produkt["Name"],
-                            "Gramm": Gramm
-                        }
-
-                        Food_type.append(Mahlzeit_produkt)
-                        print("Gramm zu Mahlzeit gespeichert.")
-
-                    else:
-                        print("Produkt existiert nicht. Bitte zuerst anlegen.")
-
-                elif Food == "2":
-                    break
-
-                else:
-                    print("Bitte eine gültige Zahl eingeben.")
-
-            if Food_type:
-                Mahlzeit = {
-                    "Typ": Meal_type,
-                    "Produkte": Food_type
-                }
-                Mahlzeiten.append(Mahlzeit)
-                print("Mahlzeit zu Mahlzeiten gespeichert.")
+    elif auswahl == "2":
+        mahlzeit_hinzufuegen()
 
     elif auswahl == "3":
-        if not Produkte:
-            print("Keine Produkte gespeichert.")
-        else:
-            print("\nProduktliste:")
-            for Produkt in Produkte:
-                print("Name:", Produkt["Name"],
-                      "| Kalorien:", Produkt["Kalorien"],
-                      "| Eiweiß:", Produkt["Eiweiß"],
-                      "| Fett:", Produkt["Fett"],
-                      "| Kohlenhydrate:", Produkt["Kohlenhydrate"])
+        produktliste_anzeigen()
 
     elif auswahl == "4":
-        if not Mahlzeiten:
-            print("Keine Mahlzeiten gespeichert.")
-        else:
-            print("\nMahlzeitenliste:")
-            for mahlzeit in Mahlzeiten:
-                print(mahlzeit["Typ"])
-                for product in mahlzeit["Produkte"]:
-                    print("  Produkt:", product["Name"],
-                          "| Gramm:", product["Gramm"])
+        mahlzeitenliste_anzeigen()
 
     elif auswahl == "5":
-        Gesamt_Kalorien = 0
-        Gesamt_Eiweiß = 0
-        Gesamt_Fett = 0
-        Gesamt_Kohlenhydrate = 0
-
-        for mahlzeit in Mahlzeiten:
-            for product in mahlzeit["Produkte"]:
-                for gespeichertes_produkt in Produkte:
-                    if gespeichertes_produkt["Name"] == product["Name"]:
-                        faktor = product["Gramm"] / 100
-                        Gesamt_Kalorien += gespeichertes_produkt["Kalorien"] * faktor
-                        Gesamt_Eiweiß += gespeichertes_produkt["Eiweiß"] * faktor
-                        Gesamt_Fett += gespeichertes_produkt["Fett"] * faktor
-                        Gesamt_Kohlenhydrate += gespeichertes_produkt["Kohlenhydrate"] * faktor
-
-        tageswerte = {
-            "Kalorien": Gesamt_Kalorien,
-            "Eiweiß": Gesamt_Eiweiß,
-            "Fett": Gesamt_Fett,
-            "Kohlenhydrate": Gesamt_Kohlenhydrate
-        }
-
-        print("\nTageswerte:")
-        for name, wert in tageswerte.items():
-            print(f"{name}: {wert:.2f}")
+        tageswerte_anzeigen()
 
     elif auswahl == "6":
         print("Programm beendet.")
